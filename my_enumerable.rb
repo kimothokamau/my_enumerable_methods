774385f1fb386526d26e9e5b47c4cfe89a2e6210
kimothokamau <kimothokamau@gmail.com>
2020-12-24 20:26:18 -0600

    Add my_each_with_index method

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -2,7 +2,16 @@ module Enumerable
   def my_each
     counter = 0
     until counter == length
-      yield self[counter]
+      yield(self[counter])
+      counter += 1
+    end
+    self
+  end
+
+  def my_each_with_index
+    counter = 0
+    until counter == length
+      yield(self[counter], counter)
       counter += 1
     end
     self
@@ -11,3 +20,6 @@ end
 
 array = [1, 2, 3, 4, 5]
 array.my_each { |e| puts "The block just got handed #{e} " }
+
+array1 = %w[kevin sylvia lucy grace]
+array1.my_each_with_index { |child, index| puts "#{child} the #{index + 1} born" }
