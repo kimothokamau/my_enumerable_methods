363d84ce94c22c0b117a33761feb61d61b214ff1
Alexis Varela <alexisbec13@hotmail.com>
2021-01-12 09:41:27 -0600

    added my_all and my_any methods

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -24,6 +24,21 @@ module Enumerable
     end
     arr
   end
+
+  def my_all?
+    my_each do |item|
+      return false unless yield(item)
+    end
+    true
+  end
+
+  def my_any?
+    my_each do |item|
+      return true if yield(item)
+    end
+    false
+  end
+
 end
 
 array = [1, 2, 3, 4, 5]
@@ -32,3 +47,7 @@ array = [1, 2, 3, 4, 5]
 # array1 = %w[kevin sylvia lucy grace]
 # array1.my_each_with_index { |child, index| puts "#{child} the #{index + 1} born" }
 p(array.my_select { | num | num.even? })
+
+p(array.my_all? { |num| num > 0 })
+
+p(array.my_any? { |num| num > 5 })
