6d100fd0cb5cbda15bf7a5b66ceab6873696db5a
kimothokamau <kimothokamau@gmail.com>
2021-01-12 09:14:49 -0600

    Added my_select method for arrays

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -16,10 +16,19 @@ module Enumerable
     end
     self
   end
+
+  def my_select
+    arr = []
+    my_each do |val|
+      arr.push(val) if yield(val)
+    end
+    arr
+  end
 end
 
 array = [1, 2, 3, 4, 5]
-array.my_each { |e| puts "The block just got handed #{e} " }
+# array.my_each { |e| puts "The block just got handed #{e} " }
 
-array1 = %w[kevin sylvia lucy grace]
-array1.my_each_with_index { |child, index| puts "#{child} the #{index + 1} born" }
+# array1 = %w[kevin sylvia lucy grace]
+# array1.my_each_with_index { |child, index| puts "#{child} the #{index + 1} born" }
+p(array.my_select { | num | num.even? })
